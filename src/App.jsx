eb65a2b52ed92d35fa8bb4d8ec030c3d37485486
Samiksha3002 <samiksha.jaiswal.bca@srwc.raisoni.net>
2025-11-19 00:21:32 -0800

    kjuhyg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-import Hero from './components/hero';
+import Hero from './components/Hero';
 import Features from './components/Features';
 
 import Testimonials from './components/Testimonials';
